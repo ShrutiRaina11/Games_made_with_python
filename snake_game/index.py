d386c2a9010f8a2b2a14a9b457a797467855d58e
ShrutiRaina11 <piya4123@gmail.com>
2024-06-14 00:20:37 +0530

    Snake eats apple

--- snake_game/index.py
+++ snake_game/index.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 import time
+from random import randint
 
 size = 40 #it same as the dimension of my snake image asa my snke image is 40*40
 
@@ -15,6 +16,11 @@ class Apple:
     #self.parent_screen.fill((111,123,244))
     self.parent_screen.blit(self.apple,(self.apple_pos_x,self.apple_pos_y)) #For inserting are drawing the things on the window surface blit() is used
     pygame.display.flip()
+
+  def move_apple(self):
+    self.apple_pos_x = randint(1,8)*size
+    self.apple_pos_y = randint(1,8)*size
+    #self.draw_apple()
 
 class Snake:
   def __init__(self,parent_screen,length):
@@ -31,6 +37,11 @@ class Snake:
       self.parent_screen.blit(self.snake,(self.snake_pos_x[i],self.snake_pos_y[i])) #For inserting are drawing the things on the window surface blit() is used
     pygame.display.flip() #For updating the changes to reflect in the screen (we can use update() also instead of flip())
   
+  def increase_snake_length(self):
+    self.snake_length += 1
+    self.snake_pos_x.append(-1)
+    self.snake_pos_y.append(-1)
+ 
   def move_up(self):
     self.direction = "up"
      
@@ -61,16 +72,26 @@ class Snake:
 class Game:
   def __init__(self):
     pygame.init() #For initializing pygame
-    self.surface = pygame.display.set_mode((1300,800)) #For initilizing the window basic step for pygame program
+    self.surface = pygame.display.set_mode((400,400)) #For initilizing the window basic step for pygame program
     self.surface.fill((111,123,244)) #Fill the background with the colour
     self.snake = Snake(self.surface,3)
     self.snake.draw_snake()
     self.apple = Apple(self.surface)
     self.apple.draw_apple()
 
+  def is_collision(self,pos_x1,pos_y1,pos_x2,pos_y2):
+    if pos_x1 >= pos_x2 and pos_x1 < pos_x2 + size:
+      if pos_y1 >= pos_y2 and pos_y1 < pos_y2 + size:
+        return True
+    return False
+  
   def play(self):
     self.snake.snake_walk()
     self.apple.draw_apple()
+
+    if self.is_collision(self.snake.snake_pos_x[0],self.snake.snake_pos_y[0],self.apple.apple_pos_x,self.apple.apple_pos_y):
+      self.apple.move_apple()
+      self.snake.increase_snake_length()
 
   def run(self):
     running = True
