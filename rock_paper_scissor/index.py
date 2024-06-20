e56d5243b5e8822e15e1723e85f7c422d4b68535
ShrutiRaina11 <piya4123@gmail.com>
2024-06-20 21:14:02 +0530

    Added background and score in the snake and apple game

--- rock_paper_scissor/index.py
+++ rock_paper_scissor/index.py
@@ -1,20 +1,19 @@
-from tkinter import *
+from tkinter import * 
 from PIL import Image, ImageTk
 from random import randint
 
+#background colour
+background_color = "#9b59b6"
 #main window
 root = Tk()
 root.title("Rock Paper Scissor")
-root.configure(background="#9b59b6")
-
-
-check_for_winner = 0
+root.configure(background=background_color)
 
 #indicators
-user_indicator_label = Label(root,font=50,text="USER",background="#9b59b6",foreground="white")
-user_indicator_label.grid(row=0,column=1)
-comp_indicator_label = Label(root,font=50,text="COMPUTER",background="#9b59b6",foreground="white")
-comp_indicator_label.grid(row=0,column=3)
+user_indicator_label = Label(root,font=("arial",20,"bold"),text="USER",background=background_color,foreground="white")
+user_indicator_label.grid(row=0,column=1,pady=(50, 10))
+comp_indicator_label = Label(root,font=("arial",20,"bold"),text="COMPUTER",background=background_color,foreground="white")
+comp_indicator_label.grid(row=0,column=3,pady=(50, 10))
 
 #pictures
 rock_user_img = ImageTk.PhotoImage(Image.open(r"rock_paper_scissor\images\rock_user.png"))
@@ -25,28 +24,46 @@ scissor_user_img = ImageTk.PhotoImage(Image.open(r"rock_paper_scissor\images\sci
 scissor_comp_img = ImageTk.PhotoImage(Image.open(r"rock_paper_scissor\images\scissor_comp.png"))
 
 #insert picture
-user_img_label = Label(root,image = scissor_user_img,background="#9b59b6")
-comp_img_label = Label(root,image = scissor_comp_img, background="#9b59b6")
-user_img_label.grid(row=1,column=0)
-comp_img_label.grid(row=1,column=4)
+user_img_label = Label(root,image = scissor_user_img,font=("arial",20,"bold"),background=background_color)
+comp_img_label = Label(root,image = scissor_comp_img,font=("arial",20,"bold"),background=background_color)
+user_img_label.grid(row=1,column=0,pady=(90, 10),padx=(10,0))
+comp_img_label.grid(row=1,column=4,pady=(90, 10),padx=(0,10))
 
 #scores
-user_score_label = Label(root,text=0,font=100,background="#9b59b6",foreground="white")
-comp_score_label = Label(root,text=0,font=100,background="#9b59b6",foreground="white")
-user_score_label.grid(row=1,column=1)
-comp_score_label.grid(row=1,column=3)
+user_score_label = Label(root,text=0,font=("arial",20,"bold"),background=background_color,foreground="white")
+comp_score_label = Label(root,text=0,font=("arial",20,"bold"),background=background_color,foreground="white")
+user_score_label.grid(row=1,column=1,pady=(50, 10))
+comp_score_label.grid(row=1,column=3,pady=(50, 10))
 
 #buttons
-rock_btn = Button(root,width=20,height=2,text="Rock",background="#FF3E4D",foreground="white",command=lambda:updatde_choice("rock"))
-rock_btn.grid(row=2,column=1)
-paper_btn = Button(root,width=20,height=2,text="Paper",background="#FAD02E",foreground="white",command=lambda:updatde_choice("paper"))
-paper_btn.grid(row=2,column=2)
-scissor_btn = Button(root,width=20,height=2,text="Scissor",background="#0ABDE3",foreground="white",command=lambda:updatde_choice("scissor"))
-scissor_btn.grid(row=2,column=3)
+rock_btn = Button(root,width=20,height=2,text="Rock",background="#FF3E4D",font=("arial",20,"bold"),foreground="black",command=lambda:updatde_choice("rock"))
+rock_btn.grid(row=2,column=1,pady=(90, 10))
+paper_btn = Button(root,width=20,height=2,text="Paper",background="#FAD02E",font=("arial",20,"bold"),foreground="black",command=lambda:updatde_choice("paper"))
+paper_btn.grid(row=2,column=2,pady=(90, 10))
+scissor_btn = Button(root,width=20,height=2,text="Scissor",background="#0ABDE3",font=("arial",20,"bold"),foreground="black",command=lambda:updatde_choice("scissor"))
+scissor_btn.grid(row=2,column=3,pady=(90, 10))
 
-#message
-msg_label = Label(root,text="",font=50,background="#9b59b6",foreground="white")
-msg_label.grid(row=3,column=2)
+#frame for reset and quit button 
+r_q_frame = Frame(root,background=background_color)
+
+#reset button
+reset_btn = Button(r_q_frame, text="Reset Game",font=("arial",20,"bold"),command=lambda: reset_game())
+reset_btn.pack(side=LEFT ,padx=10)
+
+#quit game button
+quit_btn = Button(r_q_frame, text="Quit Game",font=("arial",20,"bold"),command=lambda: quit_game())
+quit_btn.pack(side=RIGHT ,padx=10)
+
+r_q_frame.grid(row=3, column=0, rowspan=4, columnspan=5, pady=(90,10))
+
+# end game frame
+end_game_frame = Frame(root,background=background_color)
+end_game_message = Label(end_game_frame,text="",font=("arial",20,"bold"),background=background_color,foreground="white")
+end_game_message.pack(pady=10)
+playagain_btn = Button(end_game_frame,text="Play Again",font=("arial",20,"bold"),command=lambda: reset_game())
+playagain_btn.pack(pady=10)
+end_game_frame.grid(row=1, column=0, rowspan=2, columnspan=5, pady=(0,100))
+end_game_frame.grid_remove()  # Hide initially
 
 #update choices
 def updatde_choice(user_choice):
@@ -71,13 +88,7 @@ def updatde_choice(user_choice):
 
   update_score(user_choice,comp_choice)
   
-  check_for_winner += 1
-  if(check_for_winner == 10):
-    check_winner()
-  
-#update message
-def update_message(message):
-  msg_label["text"] = message
+  check_winner()
 
 #update user score
 def update_user_score():
@@ -114,11 +125,39 @@ def update_score(user_choice,comp_choice):
 def check_winner():
   user_score = int(user_score_label["text"])
   comp_score = int(comp_score_label["text"])
-  if(user_score == comp_score):
-    update_message("It's a Tie!!")
-  elif(user_score >= comp_score):
-    update_message("You Win!!")
-  elif(user_score <= comp_score):
-    update_message("Computer Win!!")
- 
+  if(user_score == 5 and comp_score == 5):
+    show_endgame_message("It's a Tie!!")
+  elif(user_score == 5):
+    show_endgame_message("You Win!!")
+  elif(comp_score == 5):
+    show_endgame_message("Computer Win!!")
+
+def disable_buttons():
+  rock_btn.configure(state = DISABLED)
+  paper_btn.configure(state = DISABLED)
+  scissor_btn.configure(state = DISABLED)
+  reset_btn.configure(state = DISABLED)
+
+def active_buttons():
+  rock_btn.configure(state = NORMAL)
+  paper_btn.configure(state = NORMAL)
+  scissor_btn.configure(state = NORMAL)
+  reset_btn.configure(state = NORMAL)
+
+def show_endgame_message(message):
+    end_game_message.config(text=message)
+    end_game_frame.grid()
+    disable_buttons()
+
+def reset_game():
+    active_buttons()
+    user_score_label["text"] = "0"
+    comp_score_label["text"] = "0"
+    user_img_label.configure(image=scissor_user_img)
+    comp_img_label.configure(image=scissor_comp_img)
+    end_game_frame.grid_remove()
+
+def quit_game():
+  exit()
+
 root.mainloop()
--- snake_game/index.py
+++ snake_game/index.py
@@ -4,6 +4,11 @@ import time
 from random import randint
 
 size = 40 #it same as the dimension of my snake image asa my snke image is 40*40
+screen_width = 1200
+screen_height = 800
+background_colour = 111,123,244
+
+fps = pygame.time.Clock() # FPS (frames per second) controller
 
 class Apple:
   def __init__(self,parent_screen):
@@ -13,14 +18,11 @@ class Apple:
     self.parent_screen = parent_screen
 
   def draw_apple(self):
-    #self.parent_screen.fill((111,123,244))
-    self.parent_screen.blit(self.apple,(self.apple_pos_x,self.apple_pos_y)) #For inserting are drawing the things on the window surface blit() is used
-    pygame.display.flip()
+    self.parent_screen.blit(self.apple,(self.apple_pos_x,self.apple_pos_y)) #For inserting are drawing the things on the window screen blit() is used
 
   def move_apple(self):
-    self.apple_pos_x = randint(1,8)*size
-    self.apple_pos_y = randint(1,8)*size
-    #self.draw_apple()
+    self.apple_pos_x = randint(1,(screen_width/size)-1)*size
+    self.apple_pos_y = randint(1,(screen_height/size)-1)*size
 
 class Snake:
   def __init__(self,parent_screen,length):
@@ -30,13 +32,12 @@ class Snake:
     self.snake_pos_y = [size]*self.snake_length
     self.parent_screen = parent_screen
     self.direction = None
+    self.snake_speed = 6
 
   def draw_snake(self):
-    self.parent_screen.fill((111,123,244))
     for i in range(self.snake_length):
-      self.parent_screen.blit(self.snake,(self.snake_pos_x[i],self.snake_pos_y[i])) #For inserting are drawing the things on the window surface blit() is used
-    pygame.display.flip() #For updating the changes to reflect in the screen (we can use update() also instead of flip())
-  
+      self.parent_screen.blit(self.snake,(self.snake_pos_x[i],self.snake_pos_y[i])) #For inserting are drawing the things on the window screen blit() is used
+
   def increase_snake_length(self):
     self.snake_length += 1
     self.snake_pos_x.append(-1)
@@ -72,26 +73,45 @@ class Snake:
 class Game:
   def __init__(self):
     pygame.init() #For initializing pygame
-    self.surface = pygame.display.set_mode((400,400)) #For initilizing the window basic step for pygame program
-    self.surface.fill((111,123,244)) #Fill the background with the colour
-    self.snake = Snake(self.surface,3)
-    self.snake.draw_snake()
-    self.apple = Apple(self.surface)
-    self.apple.draw_apple()
+    self.screen = pygame.display.set_mode((screen_width,screen_height)) #For initilizing the window basic step for pygame program
+    self.snake = Snake(self.screen,1)
+    self.apple = Apple(self.screen)
 
-  def is_collision(self,pos_x1,pos_y1,pos_x2,pos_y2):
-    if pos_x1 >= pos_x2 and pos_x1 < pos_x2 + size:
-      if pos_y1 >= pos_y2 and pos_y1 < pos_y2 + size:
-        return True
+  def render_background(self):
+    bg = pygame.image.load(r"snake_game\resources\background.jpg")
+    self.screen.blit(bg, (0,0))
+
+  def snake_collision_with_wall(self,snake_pos_x, snake_pos_y):
+    if(snake_pos_x >= (screen_width-size)  or snake_pos_x <= 0):
+      self.snake.move_down()
+    if(snake_pos_y >= (screen_height-size) or snake_pos_y <= 0):
+      self.snake.move_right()
+
+  def is_apple_eaten(self,pos_x1,pos_y1,pos_x2,pos_y2):
+    if pos_x1 == pos_x2 and pos_y1 == pos_y2 :
+      return True
     return False
   
+  def display_score(self):
+    font = pygame.font.SysFont('arial',30)
+    score = font.render(f"Score: {self.snake.snake_length}",True,(200,200,200))
+    score_rect = score.get_rect()
+    self.screen.blit(score,score_rect)
+
   def play(self):
+    self.render_background()
     self.snake.snake_walk()
     self.apple.draw_apple()
+    self.display_score()
 
-    if self.is_collision(self.snake.snake_pos_x[0],self.snake.snake_pos_y[0],self.apple.apple_pos_x,self.apple.apple_pos_y):
+    if self.is_apple_eaten(self.snake.snake_pos_x[0],self.snake.snake_pos_y[0],self.apple.apple_pos_x,self.apple.apple_pos_y):
       self.apple.move_apple()
       self.snake.increase_snake_length()
+    
+    self.snake_collision_with_wall(self.snake.snake_pos_x[0],self.snake.snake_pos_y[0])
+
+    pygame.display.flip() #For updating the changes to reflect in the screen (we can use update() also instead of flip())
+    fps.tick(self.snake.snake_speed)
 
   def run(self):
     running = True
@@ -113,8 +133,7 @@ class Game:
           running = False
 
       self.play()
-      time.sleep(0.2)
-  
+
 if __name__ == "__main__":
   game = Game()
   game.run()
